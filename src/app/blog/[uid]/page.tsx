22fa46337865be45576bdf9cfd0906656ec0d3eb
akash8590527730 <akashkurian2020@gmail.com>
2025-04-28 16:37:40 +0530

    changess

--- src/app/blog/[uid]/page.tsx
+++ src/app/blog/[uid]/page.tsx
@@ -1,14 +1,16 @@
 import { Metadata } from "next";
 import { notFound } from "next/navigation";
-
 import { createClient } from "@/prismicio";
-
 import ContentBody from "@/components/ContentBody";
 
+// This is your route param type
+type PageProps = {
+  params: {
+    uid: string;
+  };
+};
 
-type Params = { uid: string };
-
-export default async function Page({ params }: { params: Params }) {
+export default async function Page({ params }: PageProps) {
   const client = createClient();
   const page = await client
     .getByUID("blog_post", params.uid)
@@ -17,11 +19,7 @@ export default async function Page({ params }: { params: Params }) {
   return <ContentBody page={page} />;
 }
 
-export async function generateMetadata({
-  params,
-}: {
-  params: Params;
-}): Promise<Metadata> {
+export async function generateMetadata({ params }: PageProps): Promise<Metadata> {
   const client = createClient();
   const page = await client
     .getByUID("blog_post", params.uid)
@@ -37,7 +35,7 @@ export async function generateStaticParams() {
   const client = createClient();
   const pages = await client.getAllByType("blog_post");
 
-  return pages.map((page) => {
-    return { uid: page.uid };
-  });
+  return pages.map((page) => ({
+    uid: page.uid,
+  }));
 }
