2845830d22b887879953ffd52a7b24aa33373887
akash8590527730 <akashkurian2020@gmail.com>
2025-04-28 16:45:14 +0530

    chan

--- src/app/blog/[uid]/page.tsx
+++ src/app/blog/[uid]/page.tsx
@@ -3,31 +3,43 @@ import { notFound } from "next/navigation";
 import { createClient } from "@/prismicio";
 import ContentBody from "@/components/ContentBody";
 
-// This is your route param type
-type PageProps = {
+// Props type for the page
+interface PageProps {
   params: {
     uid: string;
   };
-};
+}
 
 export default async function Page({ params }: PageProps) {
   const client = createClient();
+  
   const page = await client
     .getByUID("blog_post", params.uid)
     .catch(() => notFound());
+
+  if (!page) {
+    notFound();
+  }
 
   return <ContentBody page={page} />;
 }
 
-export async function generateMetadata({ params }: PageProps): Promise<Metadata> {
+export async function generateMetadata(
+  { params }: PageProps
+): Promise<Metadata> {
   const client = createClient();
+  
   const page = await client
     .getByUID("blog_post", params.uid)
     .catch(() => notFound());
 
+  if (!page) {
+    notFound();
+  }
+
   return {
-    title: page.data.title,
-    description: page.data.meta_description,
+    title: page.data.title as string,
+    description: page.data.meta_description as string,
   };
 }
 
